864bb9d572d857e19c2cb1d017d08661952ee39d
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-12 19:23:03 +0900

    [FRONTEND] Add page transition

--- FRONTEND/src/components/navigator/tab.vue
+++ FRONTEND/src/components/navigator/tab.vue
@@ -1,13 +1,15 @@
 <template>
     <section class="border-b p-2 gap-2 flex h-10 align-baseline">
         <section class="flex items-center" v-for="item in useTabStore().getCacheList()">
-            <button class="btn btn-outline rounded-none btn-xs px-1" @click="router.push({ name: item })" :key="item">
+            <button class="btn btn-outline rounded-none btn-xs px-1 border-r-0" @click="router.push({ name: item })" :key="item">
                 {{ item }}
             </button>
+            <button class="btn btn-outline rounded-none btn-xs px-0.5 border-l-0"><XMarkIcon class="w-3" /></button>
         </section>
     </section>
 </template>
 <script setup lang="ts">
 import { router } from '../../router/router'
 import { useTabStore } from '../../store/modules/tab'
+import { XMarkIcon } from '@heroicons/vue/24/solid'
 </script>
--- FRONTEND/src/layout/keepaliverouter.vue
+++ FRONTEND/src/layout/keepaliverouter.vue
@@ -2,9 +2,11 @@
     <RouterView v-slot="{ Component, route }">
         <!-- 각 컴포넌트에 sciprt문이 하나도 없을경우 keepalive 셋팅이 불가능해보임 -->
         <!-- 당장 Card하나 임포트하고 keepalive 실행해보니 너무나 잘됨 -->
-        <KeepAlive :include="cachedView">
-            <component :is="Component" :key="route.fullPath" />
-        </KeepAlive>
+        <Transition name="scale" mode="out-in">
+            <KeepAlive :include="cachedView">
+                <component :is="Component" :key="route.fullPath" />
+            </KeepAlive>
+        </Transition>
     </RouterView>
 </template>
 <script setup lang="ts">
@@ -13,3 +15,15 @@ import { useTabStoreWithoutInit } from '../store/modules/tab'
 
 const cachedView = computed(() => useTabStoreWithoutInit().getCacheList())
 </script>
+<style>
+.scale-enter-active,
+.scale-leave-active {
+    transition: all 0.15s ease;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+    opacity: 0;
+    transform: scale(0.9);
+}
+</style>
