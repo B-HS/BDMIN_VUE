6ee80a079c351e2d22d395a051bb41d12c81d828
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-12 19:05:58 +0900

    [FRONTEND] Update layout and add component

---
+++ FRONTEND/src/components/card.vue
@@ -0,0 +1,5 @@
+<template>
+    <section class="border p-2 my-2">
+        <slot></slot>
+    </section>
+</template>
--- FRONTEND/src/layout/layout.vue
+++ FRONTEND/src/layout/layout.vue
@@ -1,6 +1,6 @@
 <template>
     <section v-if="!router.currentRoute.value.meta.isAuthed">
-        <RouterView />
+        <Keepaliverouter />
     </section>
     <section class="flex items-start" v-else>
         <Menubar />
@@ -8,14 +8,7 @@
             <Navibar />
             <Tab />
             <section class="p-3">
-                <RouterView v-slot="{ Component, route }">
-                    <!-- <KeepAlive :include="cachable"> -->
-                    <!-- TODO inlcude넣으면 캐싱이 안되는 버그가 있음 -->
-                    <!-- 몇일 더 찾아보고 안되면 issue하나 올리기 -->
-                    <KeepAlive>
-                        <component :is="Component" :key="route.fullPath" />
-                    </KeepAlive>
-                </RouterView>
+                <Keepaliverouter />
             </section>
         </section>
     </section>
@@ -26,9 +19,5 @@ import Menubar from '../components/menu/menubar.vue'
 import Navibar from '../components/navigator/navibar.vue'
 import Tab from '../components/navigator/tab.vue'
 import { router } from '../router/router'
-
-// const cachable = router
-//     .getRoutes()
-//     .filter((val) => val.meta.cache)
-//     .map((val) => val.name) as string[]
+import Keepaliverouter from './keepaliverouter.vue'
 </script>
