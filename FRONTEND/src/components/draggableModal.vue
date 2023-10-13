631ef92df80553d62097aa00adfb8918ffc592dc
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-14 04:00:17 +0900

    [FRONTEND] Add modal sample by dialog

---
+++ FRONTEND/src/components/draggableModal.vue
@@ -0,0 +1,28 @@
+<template>
+    <section>
+        <button @click="draggableModal.showModal()">??</button>
+        <dialog ref="draggableModal" class="modal">
+            <div class="modal-box">
+                <div ref="modalheader" class="modal-header">
+                    <h3 class="font-bold text-lg">Hello!</h3>
+                </div>
+                <div class="divider"></div>
+                <p>Press ESC key or click outside to close</p>
+            </div>
+            <form method="dialog" class="modal-backdrop">
+                <button>close</button>
+            </form>
+        </dialog>
+    </section>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const draggableModal = ref()
+const modalheader = ref()
+
+const show = () => draggableModal.value.showModal()
+const close = () => draggableModal.value.close()
+
+defineExpose({ show, close })
+</script>
--- FRONTEND/src/pages/system/menu.vue
+++ FRONTEND/src/pages/system/menu.vue
@@ -3,8 +3,14 @@
         <Card>
             <input value="" />
         </Card>
+        <DraggableModal ref="dgModal" />
+        <button @click="dgModal.show">SHOW</button>
+        <button @click="dgModal.close">HIDE</button>
     </section>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
 import Card from '../../components/card.vue'
+import DraggableModal from '../../components/draggableModal.vue'
+const dgModal = ref()
 </script>
