2463ce89baf524ae96eb2ca5fdae0b0f7661a205
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-14 01:09:03 +0900

    [FRONTEND] Handle menu visibility by width.

--- FRONTEND/src/components/menu/menubar.vue
+++ FRONTEND/src/components/menu/menubar.vue
@@ -1,7 +1,10 @@
 <template>
     <section
         class="h-screen transition-all overflow-hidden border-r"
-        :style="{ width: useAppstore().getMenuHide() ? '0px' : useAppstore().getMenubarWidth() + 'px' }"
+        :style="{
+            width: useAppstore().getMenuHide() ? '0px' : useAppstore().getMenubarWidth() + 'px',
+            minWidth: useAppstore().getMenuHide() ? '0px' : isUnder640 ? useAppstore().getMenubarWidth() + 'px' : 0,
+        }"
     >
         <header @click="router.push({ path: '/' })" class="bg-base-neutral-content p-3 text-center text-xl cursor-pointer">
             {{ title }}
@@ -10,11 +13,18 @@
     </section>
 </template>
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, nextTick, ref } from 'vue'
 import { getEnv } from '../../module/env'
 import { router } from '../../router/router'
 import { useAppstore } from '../../store/modules/app'
 import Menulist from './menulist.vue'
 
 const title = computed(() => getEnv('VITE_APP_TITLE')).value
+const isUnder640 = ref(false)
+const menuHide = () => !useAppstore().getMenuHide() && useAppstore().setMenuHide((isUnder640.value = true))
+const menuVisible = () => useAppstore().getMenuHide() && useAppstore().setMenuHide((isUnder640.value = false))
+nextTick(() => {
+    window.innerWidth < 640 ? menuHide() : menuVisible()
+    window.addEventListener('resize', () => (window.innerWidth < 640 ? menuHide() : menuVisible()))
+})
 </script>
