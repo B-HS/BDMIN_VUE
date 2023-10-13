db19ef1f994560ad41eeea5533c5a45f618a9406
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-14 02:58:59 +0900

    [FRONTEND] Add breadcrumble

--- FRONTEND/src/components/navigator/navibar.vue
+++ FRONTEND/src/components/navigator/navibar.vue
@@ -4,7 +4,16 @@
             <button @click="() => menuToggle()" class="btn btn-ghost btn-circle"><Bars3Icon class="text-ghost h-1/2" /></button>
         </div>
         <div class="navbar-center">
-            <a class="btn btn-ghost normal-case text-xl">{{ router.currentRoute.value.meta.title }}</a>
+            <span class="normal-case text-xl font-bold" v-if="router.currentRoute.value.meta.title === 'HOME'">{{
+                router.currentRoute.value.meta.title
+            }}</span>
+            <div class="breadcrumbs p-0">
+                <ul>
+                    <li class="overflow-hidden" v-for="item in breadcrumbs" :key="item">
+                        <span class="normal-case text-xl font-bold">{{ item }}</span>
+                    </li>
+                </ul>
+            </div>
         </div>
         <div class="navbar-end">
             <button class="btn btn-ghost btn-circle">
@@ -18,8 +27,38 @@
 </template>
 <script setup lang="ts">
 import { Bars3Icon, BellIcon, MagnifyingGlassIcon } from '@heroicons/vue/24/solid'
+import { onMounted, ref, watch } from 'vue'
 import { router } from '../../router/router'
 import { useAppstore } from '../../store/modules/app'
+import { useUserStore } from '../../store/modules/user'
+import { MenuItem } from '../../types/user'
 
+const breadcrumbs = ref()
 const menuToggle = () => useAppstore().setMenuHide(!useAppstore().getMenuHide())
+const findBreadcrumbPath = (menu: MenuItem[], targetName: string, path = ''): string[] | null => {
+    for (const item of menu) {
+        const currentPath = path === '' ? item.name : `${path}/${item.meta.title}`
+        if (item.name === targetName) {
+            return currentPath.split('/')
+        }
+
+        if (item.children) {
+            const result = findBreadcrumbPath(item.children, targetName, currentPath)
+            if (result) {
+                return result && result
+            }
+        }
+    }
+    return null
+}
+onMounted(() => {
+    findBreadcrumbPath(useUserStore().getRefectoredMenu() as MenuItem[], router.currentRoute.value.name as string)
+})
+
+watch(
+    () => router.currentRoute.value,
+    (newval) => {
+        breadcrumbs.value = findBreadcrumbPath(useUserStore().getRefectoredMenu() as MenuItem[], newval.name as string)
+    },
+)
 </script>
