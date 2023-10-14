c0ef09d43da225b7f6fff0714c3d1a51dc6d9c22
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-14 17:23:20 +0900

    [FRONTEND] Add active to menu by current path

--- FRONTEND/src/components/menu/menuitem.vue
+++ FRONTEND/src/components/menu/menuitem.vue
@@ -1,18 +1,21 @@
 <template>
     <li v-if="!item.hide">
         <details v-if="item.children && item.children.length > 0" open>
-            <summary>{{ item.meta.title }}</summary>
+            <summary class="font-bold text-lg">{{ item.meta.title }}</summary>
             <ul>
                 <menuitem v-for="childItem in item.children" :key="childItem.mekey" :item="childItem" />
             </ul>
         </details>
-        <router-link v-else-if="item.path" :to="item.path">{{ item.meta.title }}</router-link>
+        <router-link :class="[{ active: router.currentRoute.value.path === item.path }]" v-else-if="item.path" :to="item.path">{{
+            item.meta.title
+        }}</router-link>
         <span v-else>{{ item.meta.title }}</span>
     </li>
 </template>
 
 <script setup lang="ts">
 import { defineProps } from 'vue'
+import { router } from '../../router/router'
 
 const { item } = defineProps(['item'])
 </script>
