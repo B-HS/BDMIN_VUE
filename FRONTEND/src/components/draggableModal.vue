8c301ef4c508fb29e3f18d4f632a2a34c64fdfd7
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-14 16:09:10 +0900

    [FRONTEND] Fix modal to composable and draggable

--- FRONTEND/src/components/draggableModal.vue
+++ FRONTEND/src/components/draggableModal.vue
@@ -1,28 +1,67 @@
 <template>
-    <section>
-        <button @click="draggableModal.showModal()">??</button>
-        <dialog ref="draggableModal" class="modal">
-            <div class="modal-box">
-                <div ref="modalheader" class="modal-header">
-                    <h3 class="font-bold text-lg">Hello!</h3>
-                </div>
-                <div class="divider"></div>
-                <p>Press ESC key or click outside to close</p>
-            </div>
-            <form method="dialog" class="modal-backdrop">
-                <button>close</button>
-            </form>
-        </dialog>
-    </section>
+    <dialog ref="draggableModal" class="modal">
+        <div class="modal-box p-0">
+            <section
+                ref="modalheader"
+                class="modal-header flex justify-between p-5 select-none"
+                @mousedown="startDrag"
+                @mousemove="drag"
+                @mouseup="stopDrag"
+            >
+                <h3 class="font-bold text-xl">{{ props.title }}</h3>
+                <button><XMarkIcon class="w-7" @click="close" /></button>
+            </section>
+
+            <section class="modal-body p-5 px-7">
+                <slot name="body"></slot>
+            </section>
+
+            <section class="modal-footer px-5 py-3 flex justify-end" v-if="props.footer">
+                <button v-for="item in props.footer" :key="item.name" :class="['btn', item.class, `btn-${item.type}`]" @click="item.fn">
+                    {{ item.name }}
+                </button>
+            </section>
+        </div>
+        <form method="dialog" class="modal-backdrop">
+            <button></button>
+        </form>
+    </dialog>
 </template>
-<script setup lang="ts">
-import { ref } from 'vue'
+<script setup lang="ts" generic="T extends string | number, U extends { footer: T[] | U; body: T | U }">
+import { XMarkIcon } from '@heroicons/vue/24/solid'
+import { reactive, ref } from 'vue'
 
 const draggableModal = ref()
 const modalheader = ref()
-
+const props = defineProps<{ title?: string; footer?: { name: string; type: string; fn: Function; class?: string }[] }>()
 const show = () => draggableModal.value.showModal()
 const close = () => draggableModal.value.close()
+const modalStatus = reactive({
+    isDragging: false,
+    offsetX: 0,
+    offsetY: 0,
+})
+
+const startDrag = (event: MouseEvent) => {
+    modalStatus.isDragging = true
+    modalStatus.offsetX = event.clientX - draggableModal.value.getBoundingClientRect().left
+    modalStatus.offsetY = event.clientY - draggableModal.value.getBoundingClientRect().top
+    window.addEventListener('mousemove', drag)
+}
+
+const drag = (event: MouseEvent) => {
+    if (modalStatus.isDragging) {
+        const x = event.clientX - modalStatus.offsetX
+        const y = event.clientY - modalStatus.offsetY
+        draggableModal.value.style.left = `${x}px`
+        draggableModal.value.style.top = `${y}px`
+    }
+}
+
+const stopDrag = () => {
+    modalStatus.isDragging = false
+    window.removeEventListener('mousemove', drag)
+}
 
 defineExpose({ show, close })
 </script>
--- FRONTEND/src/pages/system/menu.vue
+++ FRONTEND/src/pages/system/menu.vue
@@ -3,14 +3,8 @@
         <Card>
             <input value="" />
         </Card>
-        <DraggableModal ref="dgModal" />
-        <button @click="dgModal.show">SHOW</button>
-        <button @click="dgModal.close">HIDE</button>
     </section>
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
 import Card from '../../components/card.vue'
-import DraggableModal from '../../components/draggableModal.vue'
-const dgModal = ref()
 </script>
