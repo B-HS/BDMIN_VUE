bdef42244c4ef922c6c9ce377477c77ebe0364fe
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-18 01:43:09 +0900

    [FRONTEND] Add save/delete api for locale

--- FRONTEND/src/api/locale/index.ts
+++ FRONTEND/src/api/locale/index.ts
@@ -1,3 +1,5 @@
 import { axios } from '../../module/axios'
 
 export const requestFullLocaleList = async () => axios.post('/locale')
+export const saveLocale = async (locale: object[]) => axios.post('/locale/save', [...locale])
+export const deleteLocale = async (locale: (string | undefined)[]) => axios.post('/locale/delete', [...locale])
