be65d70a287fa0e8c515122028e3256842eb9079
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-20 20:11:03 +0900

    [FRONTEND] Implement editable table with rowstatus

--- FRONTEND/src/pages/system/menu.vue
+++ FRONTEND/src/pages/system/menu.vue
@@ -1,10 +1,116 @@
 <template>
     <section>
-        <Card>
-            <input value="" />
+        <Card class="flex gap-2 justify-end">
+            <button class="btn btn-outline btn-xs" @click="addRow">{{ t('ADD') }}</button>
+            <button class="btn btn-outline btn-xs" @click="rowDelete">{{ t('DELETE') }}</button>
+            <button class="btn btn-outline btn-xs" @click="save">{{ t('SAVE') }}</button>
+            <button class="btn btn-outline btn-xs" @click="search">{{ t('SEARCH') }}</button>
+        </Card>
+        <Card class="overflow-x-auto">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th v-for="column in columns" :key="column">{{ t(column.toUpperCase()) }}</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, idx) in state.tableData" class="hover:bg-base-200">
+                        <EditableTd
+                            v-for="elements in columns"
+                            :content="state.tableData[idx][elements as keyof MenuHeader]"
+                            :contenteditable="state.tableData[idx]['row_status'] !== 'D'"
+                            :class="[{ 'opacity-20 cursor-not-allowed line-through': state.tableData[idx]['row_status'] === 'D' }]"
+                            @input="(data) => changeEvent(data, idx, elements as keyof MenuHeader)"
+                            @click="() => setCurrentCell(idx)"
+                            :key="elements + idx"
+                        >
+                            {{ item.mekey }}
+                        </EditableTd>
+                    </tr>
+                </tbody>
+            </table>
         </Card>
     </section>
 </template>
 <script setup lang="ts">
+import { reactive } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { requestMenuList } from '../../api/menu'
 import Card from '../../components/card.vue'
+import EditableTd from '../../components/table/editableTd.vue'
+
+interface MenuHeader {
+    idx?: number
+    mekey: number
+    mename: string
+    parentmekey: number
+    path: string
+    file: string
+    meorder: number
+    hide: boolean
+    cache: boolean
+    icon: string
+    row_status?: 'I' | 'U' | 'D'
+}
+
+const { t } = useI18n()
+const columns = ['mekey', 'mename', 'parentmekey', 'meorder', 'hide', 'cache', 'icon']
+const state = reactive({
+    tableData: [] as Partial<MenuHeader>[],
+    orgTableData: [] as Partial<MenuHeader>[],
+    currentCellInfo: {} as Partial<MenuHeader>,
+})
+const search = async () => {
+    state.tableData = []
+    state.orgTableData = []
+    const { data } = await requestMenuList()
+    state.tableData = JSON.parse(JSON.stringify(data))
+    state.orgTableData = JSON.parse(JSON.stringify(data))
+}
+const save = () => {
+    getChangedDataWithStatus()
+    console.log('data', state.tableData)
+    console.log('orgData', state.orgTableData)
+}
+
+const addRow = () => {
+    state.tableData = [...state.tableData, { row_status: 'I' }]
+}
+
+const rowDelete = () => {
+    console.log(state.currentCellInfo.idx)
+
+    if (state.currentCellInfo.idx !== undefined) {
+        console.log(state.currentCellInfo.idx)
+        state.tableData[state.currentCellInfo.idx] = { ...state.currentCellInfo, row_status: 'D', idx: undefined }
+    }
+    state.currentCellInfo = {}
+}
+
+const changeEvent = (newContent: any, idx: number, target: keyof MenuHeader) => {
+    state.tableData[idx][target] = newContent
+}
+
+const getChangedDataWithStatus = () => {
+    state.tableData.forEach((item, index) => {
+        const orgItem = state.orgTableData[index]
+        if (orgItem && !isDataEqual(item, orgItem)) {
+            item.row_status = 'U'
+        }
+    })
+}
+
+const isDataEqual = (item1: Partial<MenuHeader>, item2: Partial<MenuHeader>): boolean => {
+    let isEqual = true
+    Object.keys(item1).forEach((key) => {
+        if (key !== 'row_status' && item1[key as keyof MenuHeader] !== item2[key as keyof MenuHeader]) {
+            isEqual = false
+        }
+    })
+    return isEqual
+}
+
+const setCurrentCell = (idx: number) => {
+    state.currentCellInfo = { idx, ...state.tableData[idx] }
+}
 </script>
