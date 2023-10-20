96c070ec9c51d9e70ac096f374db6e347689f91f
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-20 20:10:24 +0900

    [FRONTEND] Implement editable td element

---
+++ FRONTEND/src/components/table/editableTd.vue
@@ -0,0 +1,18 @@
+<template>
+    <td contenteditable="true" v-text="innerContent" @input="onInput"></td>
+</template>
+
+<script setup lang="ts">
+import { defineEmits, defineProps, ref } from 'vue'
+
+const { content } = defineProps<{ content: string | number | boolean | undefined }>()
+const emits = defineEmits(['input'])
+
+const innerContent = ref(content)
+
+const onInput = (event: InputEvent) => {
+    // @ts-ignore
+    innerContent.value = event.target?.innerHTML
+    emits('input', innerContent.value)
+}
+</script>
