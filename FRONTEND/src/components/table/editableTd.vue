2b631cc77281582c7799e2abda0a6d779099d83a
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-22 01:28:28 +0900

    [FRONTEND] Remove unused import

--- FRONTEND/src/components/table/editableTd.vue
+++ FRONTEND/src/components/table/editableTd.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script setup lang="ts">
-import { defineEmits, defineProps, ref } from 'vue'
+import { ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
