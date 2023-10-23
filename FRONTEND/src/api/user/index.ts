c60cb1873ccaee2f196665c89e4ae07a2d74800e
Hyunseok Byun <gumyoincirno@gmail.com>
2023-10-23 21:25:16 +0900

    [FRONTEND] Add user api for crud

--- FRONTEND/src/api/user/index.ts
+++ FRONTEND/src/api/user/index.ts
@@ -1,3 +1,5 @@
 import { axios } from '../../module/axios'
 
 export const requestLoginInfo = async (email: string, pw: string) => axios.post('/login', { email, pw })
+export const requestUserList = async (param?: object) => axios.post('/user/list', { ...param })
+export const saveUserList = async (param: object[]) => axios.post('/user/save', param)
